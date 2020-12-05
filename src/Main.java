b94d63051e3ae877ce53bd8bc24cebaeb16cab0b
Shreeya Gad <shreeyagad@MacBook-Pro.local>
2020-12-05 13:10:39 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,23 @@
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-//		System.out.println("OOPLang Interpreter\n");
-//		Scanner sc = new Scanner(System.in);
-//		System.out.print(">> ");
-//		String input = sc.nextLine();
-//		Environment env = new Environment();
-//		while (!input.equals("end")) {
-//			try {
-//				if (!input.equals("")) System.out.println(Interpreter.evaluate(input, env));
-//			} catch (Exception e) {
-//				System.out.println("Invalid Expression!");
-//			}
-//			System.out.println();
-//			System.out.print(">> ");
-//			input = sc.nextLine();
-//		}
-
-		 String source = "class myClass { "
-		 		+ "int num; boolean bool; "
-		 		+ "function myClass(int n, boolean b) {"
-		 		+ "num = n; bool = b; "
-		 		+ "}; "
-		 		+ "function addAttr() { return num + num; }; "
-		 		+ "}; "
-		 		+ "myClass c = new myClass(2, true); "
-		 		+ "myClass c2 = new myClass(3, false); "
-		 		+ "print(c.addAttr());"
-		 		+ "print(c2.addAttr());"
-		 		+ "print(c.num);"
-		 		+ "print(c2.num);";
-//		 Tokenizer t = new Tokenizer(source);
-//		 List<Token> tokens = t.tokenize();
-//		 System.out.println(tokens);
-//		 Parser p = new Parser(t);
-//		 System.out.println(p.parseTokens());
-		 Environment env = new Environment();
-		 System.out.println(Interpreter.evaluate(source, env));
-		
-		
-		
-		
-		
-
+		System.out.println("OOPLang Interpreter\n");
+		Scanner sc = new Scanner(System.in);
+		System.out.print(">> ");
+		String input = sc.nextLine();
+		Environment env = new Environment();
+		while (!input.equals("end")) {
+			try {
+				if (!input.equals("")) System.out.println(Interpreter.evaluate(input, env));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			System.out.println();
+			System.out.print(">> ");
+			input = sc.nextLine();
+		}
 	}
 
 }
